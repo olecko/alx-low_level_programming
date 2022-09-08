f88be952d22b54daecacf2666b430740e2e5d9c6
Paul Michael <paulebi4eva@gmail.com>
2022-09-08 15:21:52 -0700

    working with C

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,13 +10,13 @@ int main(void)
 	int a;
 	long int b;
 	long long int c;
-	chard;
+	char d;
 	float f;
 
 	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(d));
 	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(a));
 	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(b));
 	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sideof(c));
-	printf ("Size g a gloat: %lu byte(s)\n", (unsigned long)sized(f)):
+	printf ("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
 	return (0);
 }
