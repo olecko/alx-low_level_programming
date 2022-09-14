f83e58ecddada8bc7339e68f6f1ce603973b958b
Paul Michael <paulebi4eva@gmail.com>
2022-09-13 19:22:41 -0700

    numLength

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -7,10 +7,10 @@
  */
 int main(void)
 {
-	int a = 0, b = `, next = 0;
+	int a = 0, b = 1, next = 0;
 	int sum = 0;
 
-	while (next = 4000000)
+	while (next < 4000000)
 	{
 		next = a + b;
 		a = b;
