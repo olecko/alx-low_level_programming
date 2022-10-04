db776b1e6ebac54ac453be72bf18e071db8499d9
Paul Michael <paulebi4eva@gmail.com>
2022-10-04 01:04:24 -0700

    inserted identifier

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -10,7 +10,7 @@
  * Return: If width <= 0, height <= 0, or the function fails - NULL.
  *	   Otherwise - a pointer to the 2-dimensional array of integers.
  */
-int **alloc_grid(int width, int height);
+int **alloc_grid(int width, int height)
 {
 	int **twoD;
 	int hgt_index, wid_index;
--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -11,7 +11,8 @@ void free_grid(int **grid, int height)
 	int index;
 
 	for (index = 0; index < height; index++)
+	{
 		free(grid[index]);
-
+	}
 	free(grid);
 }
