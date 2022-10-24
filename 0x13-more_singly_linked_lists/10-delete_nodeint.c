1ca4e845e4d40bceb5f7e7d62a2568867fc48b1a
Paul Michael <paulebi4eva@gmail.com>
2022-10-24 11:58:38 -0700

    corrected trailing whitespaces

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -5,7 +5,7 @@
  * of a linked list.
  * @head: head of a list.
  * @index: index of the list where the node is deleted.
- * 
+ *
  * Return: 1 if it succeeded, -1 if it failed.
  */
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
